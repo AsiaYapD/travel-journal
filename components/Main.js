bd1d4d41edab0bf3f4e77ef6fb255bce08d98eee
Asia Yap-Dubois <asiayapd@gmail.com>
2023-12-24 09:38:57 -0500

    Deleted out comments

--- components/Main.js
+++ components/Main.js
@@ -18,17 +18,3 @@ export default function Main(props) {
         </div>
     )
 }
-
-
-
-/*      
-    <div className="card--stats">
-        <img src="../images/star.png" className="card--star" />
-        <span>{props.stats.rating}</span>
-        <span className="gray">({props.stats.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
-    </div>
-    <p className="card--title">{props.title}</p>
-    <p className="card--price">
-        <span className="bold">From ${props.price}</span> / person
-    </p> */

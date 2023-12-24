513208ac3df287f20e174698c6f65f5236ff2562
Asia Yap-Dubois <asiayapd@gmail.com>
2023-12-24 10:02:26 -0500

    Fixing the static data

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export default [
     {
-        id: "1",
-        title: "NAFPLIO",
-        location: "Greece",
+        selection: "1",
+        title: "Nafplio",
+        location: "GREECE",
         googleMapsUrl: "https://maps.app.goo.gl/a8wj1rxBfbFPSmfN9",
         startDate: "24 Aug, 2023",
         endDate: "28 Aug, 2023",
@@ -10,22 +10,22 @@ export default [
         image: "nafplio.jpg"
     },
     {
-        id: "2",
-        title: "ATHENS",
-        location: "Greece",
+        selection: "2",
+        title: "Athens",
+        location: "GREECE",
         googleMapsUrl: "https://maps.app.goo.gl/fzk1NSXrZNUY4EHH8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "28 Aug, 2023",
+        endDate: "29 Aug, 2023",
         description: "We stayed one night in downtown Athens and dined at Ithaki, one of my favorite restaurants in the world",
         image: "athens.jpg"
     },
     {
-        id: "3",
-        title: "CRETE",
-        location: "Greece",
+        selection: "3",
+        title: "Crete",
+        location: "GREECE",
         googleMapsUrl: "https://maps.app.goo.gl/3kHBAGY6n4Hub2Wd9",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "30 Aug, 2023",
+        endDate: "5 Sept, 2023",
         description: "We ended our trip in Crete, staying in Chania. The food was excellent, but the driving on the cliffside was horrifying!",
         image: "crete.jpg"
     }
